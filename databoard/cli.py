b53e8902e770b80f993c087e33bfe5237d6705f0
Lewin <57636993+lewinkoon@users.noreply.github.com>
2024-07-09 08:19:11 +0200

    rename pages directory

--- databoard/cli.py
+++ databoard/cli.py
@@ -1,4 +1,54 @@
-import streamlit.web.cli as cli
+import streamlit.web.cli as web
+import click
+import os
+import re
+import pandas as pd
 
-def main():
-    cli.main_run(["databoard/run.py"])
+
+@click.group(
+    epilog="Check out readme at https://github.com/lewinkoon/databoard for more details."
+)
+def cli():
+    """
+    Deploy an interactive webapp to show data results from scientific experiments.
+    """
+    pass
+
+
+@cli.command(help="Serve application.")
+def run():
+    web.main_run(["databoard/run.py"])
+
+
+@cli.command(help="Split multi-table csv file.")
+@click.argument("file", required=True, type=click.Path(exists=True))
+def split(file):
+    with open(file, "r") as f:
+        lines = f.readlines()
+
+    datatable = {}
+    buffer = []
+    counter = 1
+    name = ""
+    delimiter = ";"
+
+    for line in lines:
+        if "[Name]" in line:
+            if buffer:
+                df = pd.DataFrame([x.split(delimiter) for x in buffer])
+                output = f"{counter}.csv"
+                df.to_csv(output, index=False, header=False)
+                counter += 1
+                buffer = []
+
+            name = lines[lines.index(line) + 1].strip().split(delimiter)[0]
+        elif "[Data]" in line:
+            raw_header = lines[lines.index(line) + 1].strip().split(delimiter)[0]
+            header = re.sub(r"\s*\[.*?\]\s*", "", raw_header)
+        elif any(char != delimiter for char in line.strip()):
+            buffer.append(line.strip())
+
+    if buffer:
+        df = pd.DataFrame([x.split(delimiter) for x in buffer])
+        output = f"{counter}.csv"
+        df.to_csv(output, index=False, header=False)
